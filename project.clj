f09a0265504054fa818ff4fb5ba8fc5e6b9d0ccf
Toby Crawley <toby@tcrawley.org>
2013-09-17 12:35:36 -0400

    Add sonatype repo until mod-lang-clojure 0.2.0 is released.

--- project.clj
+++ project.clj
@@ -5,11 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1885"]
-                 [cheshire "5.2.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [io.vertx/clojure-api "0.2.0-SNAPSHOT"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
                  [enfocus "2.0.0-beta1"]]
+  :repositories [["sonatype snapshots"
+                  "https://oss.sonatype.org/content/repositories/snapshots"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
   :immutant {:init demo.init/init
              :context-path "/"}
