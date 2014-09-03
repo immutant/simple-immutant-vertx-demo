6527a86a2f4d7733cf73fd0d7e392bb74217c9c5
Toby Crawley <toby@tcrawley.org>
2014-09-02 22:01:20 -0400

    Updates to work with latest cljs and mod-lang-clojure.

--- project.clj
+++ project.clj
@@ -3,14 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1885"]
-                 [io.vertx/clojure-api "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [io.vertx/clojure-api "1.0.3-SNAPSHOT"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
-                 [enfocus "2.0.0-beta1"]]
-  :plugins [[lein-cljsbuild "0.3.2"]]
+                 [enfocus "2.1.0"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :immutant {:init demo.init/init
              :context-path "/"}
   :cljsbuild {:builds [{:source-paths ["src-cljs"]
-                        :compiler {:output-to "resources/public/client.js"}}]})
+                        :compiler {:output-to "resources/public/client.js"}}]}
+  :repositories [["sonatype snapshots"
+                  "https://oss.sonatype.org/content/repositories/snapshots/"]])
